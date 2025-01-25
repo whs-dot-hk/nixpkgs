eb2a1b3c2f249aaab8d635c1762f050cc6968ef3
whs <hswongac@gmail.com>
2025-01-25 11:12:44 +0800

    Test

--- pkgs/build-support/rust/fetch-cargo-tarball/default.nix
+++ pkgs/build-support/rust/fetch-cargo-tarball/default.nix
@@ -144,7 +144,7 @@ stdenv.mkDerivation (
       # always install it rather than trying to write a standard default template.
       install -D $CARGO_CONFIG $name/.cargo/config
 
-      ln -s .cargo/config.toml .cargo/config
+      ln -s $name/.cargo/config.toml .cargo/config
 
       runHook postBuild
     '';
